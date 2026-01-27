29fa90cfd9d0c31949c1fa616fdabf9b51791b8b
DmytroLysachenko <dlysachenko98@gmail.com>
2026-01-27 23:58:34 +0100

    feat: divided test files into api groups

--- apps/api/src/features/job-offers/job-offers.module.ts
+++ apps/api/src/features/job-offers/job-offers.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { GeminiModule } from '@/common/modules/gemini/gemini.module';
+
 import { JobOffersController } from './job-offers.controller';
 import { JobOffersService } from './job-offers.service';
 
 @Module({
+  imports: [GeminiModule],
   controllers: [JobOffersController],
   providers: [JobOffersService],
 })
