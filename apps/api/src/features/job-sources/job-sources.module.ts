64701ec4bb4109a41f22967b11f72af92841eb72
DmytroLysachenko <dlysachenko98@gmail.com>
2026-04-15 16:48:09 +0200

    feat: modularize JobSourcesService - Part 3: diagnostics & forensics

--- apps/api/src/features/job-sources/job-sources.module.ts
+++ apps/api/src/features/job-sources/job-sources.module.ts
@@ -7,11 +7,19 @@ import { JobSourcesController } from './job-sources.controller';
 import { JobSourcesService } from './job-sources.service';
 import { JobSourcesCoreService } from './job-sources-core.service';
 import { JobSourcesLifecycleService } from './job-sources-lifecycle.service';
+import { JobSourcesDiagnosticsService } from './job-sources-diagnostics.service';
+import { RunDiagnosticsSummaryCache } from './run-diagnostics-summary-cache';
 
 @Module({
   imports: [AuthModule, JobOffersModule],
   controllers: [JobSourcesController],
-  providers: [JobSourcesService, JobSourcesCoreService, JobSourcesLifecycleService],
-  exports: [JobSourcesService, JobSourcesCoreService, JobSourcesLifecycleService],
+  providers: [
+    JobSourcesService,
+    JobSourcesCoreService,
+    JobSourcesLifecycleService,
+    JobSourcesDiagnosticsService,
+    RunDiagnosticsSummaryCache,
+  ],
+  exports: [JobSourcesService, JobSourcesCoreService, JobSourcesLifecycleService, JobSourcesDiagnosticsService],
 })
 export class JobSourcesModule {}
